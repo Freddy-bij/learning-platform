9bf02e861f045839d73c6fc1f9c7938546aa4cca
Freddy Bijanja <freddybijanja31@gmail.com>
2024-11-12 12:53:36 +0200

    leaning set-up

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () =>{
+    return(
+        <>
+     <section className="bg-[#130f40] text-white border-t-2 border-gray-600">
+      <div className=" w-1/2 mx-auto  text-center p-10 text-2xl" >  
+     <h2>Take your skills to the next level</h2>
+      <p>choose the right course to make your dream come true</p>
+      </div>
+   
+     </section>
+     </>
+    )
+}
+export default Hero;
